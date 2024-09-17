d5de4462339c53926bbc19b1dae0c6f6770feb76
Fioshi <guilhermefiochi@outlook.com>
2024-09-17 08:59:59 -0300

    Realizada mudanças para versão mobile

--- src/components/loginHeader/login_header.jsx
+++ src/components/loginHeader/login_header.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
+import '../loginHeader/login_header.css'
 
-function LoginHeader({ title, subtitle }) {
+function LoginHeader({ title, subtitle, alt }) {
     return (
         <>
             <header>
                 <h1
-                    className='p-2 fs-2'>
+                    className='h1-header fw-bolder p-2 fs-2'>
                     <Image
                         src='src\assets\usuarios-alt.png'
-                        className='m-1' />
+                        className='m-1'
+                        alt={alt} />
                     {title}
                 </h1>
                 <h2
-                    className='p-1 fs-4'>
+                    className='h2-header fw-bolder p-1 fs-4'>
                     {subtitle}
                 </h2>
             </header>
