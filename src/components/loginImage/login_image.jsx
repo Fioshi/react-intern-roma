805c69126b8df42bd7ee76c3eb10320238b564c7
Fioshi <guilhermefiochi@outlook.com>
2024-09-17 09:01:55 -0300

    Adicionado alt e retirando sublinhado de links

--- src/components/loginImage/login_image.jsx
+++ src/components/loginImage/login_image.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Col from 'react-bootstrap/Col';
 import { Image } from 'react-bootstrap';
 import "../loginImage/login_image.css"
-function ImageForm({caminhoImagem}) {
+
+function ImageForm({caminhoImagem, alt}) {
     return (
         <Col
             sm={8}
             className="image-col">
-            <Image
+            <Image  
                 src={caminhoImagem}
                 fluid
-                className="full-height-image" />
+                className="full-height-image"
+                alt={alt} />
         </Col>
     );
 }
--- src/components/pages/loginPage.jsx
+++ src/components/pages/loginPage.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import FormLogin from '../loginForm/form';
+import FormLogin from '../loginForm/login_form';
 import ImageForm from '../loginImage/login_image';
 import LoginHeader from '../loginHeader/login_header';
+import './login_page.css';
 
 function MyComponent() {
     return (
         <main>
             <Container fluid>
                 <Row
-                    className='justify-content-center'>
+                    className=' justify-content-center'>
                     <ImageForm
-                        caminhoImagem={'src/assets/pexels-paul-loh-65233-233698.jpg'}>
+                        caminhoImagem={'src/assets/pexels-paul-loh-65233-233698.jpg'}
+                        alt={"Imagens de prédios da tela de login"}>
                     </ImageForm>
                     <Col
                         sm={4}
-                        className='d-flex flex-column justify-content-center align-items-center'>
+                        className='col-header d-flex flex-column justify-content-center align-items-center'>
                         <section
                             className='w-75 justify-content-center align-items-center'>
                             <LoginHeader
@@ -30,6 +32,7 @@ function MyComponent() {
                                     className='fs-6'>
                                     Don't have an account?
                                     <a
+                                    className="text-decoration-none"
                                         href="">Sign up now
                                     </a>
                                 </p>
